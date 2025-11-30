8b92641a4869670145d36df52cb8eb2c39ac346f
LakshanWC <lakshanwc@gmail.com>
2025-11-30 15:30:48 +0530

    implemented ssl certification details checker
    implemented helper class to get ssl certification details from a url

--- app/src/main/java/com/wclw/argueseye/MainActivity.java
+++ app/src/main/java/com/wclw/argueseye/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,7 +22,10 @@ import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
+    private UrlParser urlParser;
     private EditText editText_url;
+    private LinearLayout sslCertLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +38,14 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        urlParser = new UrlParser();
+
+
         Button btn = findViewById(R.id.btn_block);
         editText_url = findViewById(R.id.editTxt_url);
+        sslCertLayout = findViewById(R.id.ssl_cert_container);
         btn.setActivated(false);
+
 
         BloomFilterHelper.initialize(this);
 
@@ -50,8 +59,16 @@ public class MainActivity extends AppCompatActivity {
 
     public void verifyButtonClick(View view){
         Button button = findViewById(R.id.btn_verify);
-        TextView tv_prefix = findViewById(R.id.tv_domain_prefix);
         TextView tv_risk = findViewById(R.id.tv_risk_level);
+
+        TextView tv_domain = findViewById(R.id.tv_domain);
+        TextView tv_scheme = findViewById(R.id.tv_scheme);
+        TextView tv_subdomain = findViewById(R.id.tv_subdomain);
+        TextView tv_tdl = findViewById(R.id.tv_tld);
+        TextView tv_path = findViewById(R.id.tv_path);
+        TextView tv_query = findViewById(R.id.tv_query);
+
+        TextView tv_cert_Stat = findViewById(R.id.tv_cert_avilability);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
 
 
                 FilterType type = BloomFilterHelper.checkFilter(editText_url.getText().toString());
+                UrlParser.Parts parts = urlParser.parseUrl(editText_url.getText().toString());
 
                 switch(type){
                     case TRUSTED:
@@ -72,9 +90,53 @@ public class MainActivity extends AppCompatActivity {
                         break;
                 }
 
-                //ToDo:implement url verification
+                if(parts != null) {
+                    tv_domain.setText(parts.domain+"." + parts.tld);
+                    tv_subdomain.setText(parts.subdomain);
+                    tv_tdl.setText(parts.tld);
+                    tv_path.setText(parts.path);
+                    tv_query.setText(parts.query);
+                    tv_scheme.setText(parts.scheme);
+
+                    if(parts.scheme.equalsIgnoreCase("https")){
+                        showCertificateDetails(editText_url.getText().toString());
+                        tv_cert_Stat.setVisibility(8);
+                    }else{
+                        tv_cert_Stat.setText("No SSL certificate available (not HTTPS)");
+                        tv_cert_Stat.setVisibility(0);
+                    }
+                }
+                else{
+                    tv_cert_Stat.setText("Invalid URL");
+                    tv_cert_Stat.setVisibility(View.VISIBLE);
+                }
             }
         });
+    }
+
+    private void showCertificateDetails(String url){
+        SSLVerificationHelper sslVerificationHelper = new SSLVerificationHelper();
+        SSLVerificationHelper.CertificateParts certPart = new SSLVerificationHelper.CertificateParts();
+        certPart = sslVerificationHelper.checkSSLCertificate(url,this);
+
+        if(certPart != null){
+
+            Toast tts = Toast.makeText(this,"i got somthing",Toast.LENGTH_SHORT);
+            tts.show();
+
+        TextView cert_subject = findViewById(R.id.tv_cert_subject);
+        TextView cert_issuer = findViewById(R.id.tv_cert_issuer);
+        TextView cert_validFrom = findViewById(R.id.tv_cert_valid_from);
+        TextView cert_validTill = findViewById(R.id.tv_cert_valid_until);
+
+            cert_subject.setText(certPart.subject != null ? certPart.subject : "N/A");
+            cert_issuer.setText(certPart.issuer != null ? certPart.issuer : "N/A");
+            cert_validFrom.setText(certPart.validFrom != null ? certPart.validFrom : "N/A");
+            cert_validTill.setText(certPart.validUntil != null ? certPart.validUntil : "N/A");
+
+        sslCertLayout.setVisibility(0);
+        }
+        else sslCertLayout.setVisibility(8);
     }
 
     public void openBrowserSandBox(View view){
